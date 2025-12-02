4bae25d1dbe9b29b3f0b5a3a1c3d10c1e5860344
Jacob Gaumond <jacobgaumond@hotmail.com>
2025-12-01 20:34:49 -0500

    Initial DML file

---
+++ sql/DML.sql
@@ -0,0 +1,70 @@
+INSERT INTO Members (member_id, first_name, last_name, email) VALUES
+    (1, 'John', 'Doe', 'john.doe@example.com'),
+    (2, 'Jane', 'Smith', 'jane.smith@example.com'),
+    (3, 'Jim', 'Beam', 'jim.beam@example.com'),
+    (4, 'Adam', 'Locke', 'adam.locke@example.com'),
+    (5, 'Brandon', 'Curie', 'bradon.curie@example.com');
+
+INSERT INTO HealthInfo (member_id, heart_rate_bpm, current_weight_lb) VALUES
+    (1, 80, 150),
+    (2, 74, 130),
+    (3, 120, 210),
+    (4, 143, 180),
+    (5, 90, 105);
+
+INSERT INTO FitnessGoals (member_id, target_weight_lb) VALUES
+    (1, 140),
+    (2, 115),
+    (3, 150),
+    (4, 140),
+    (5, 125)
+
+INSERT INTO Trainers (trainer_id, first_name, last_name) VALUES
+    (1, 'Joseph', 'Harper'),
+    (2, 'Jackson', 'Smith'),
+    (3, 'Riley', 'Lincoln'),
+    (4, 'Ashley', 'Hudson'),
+    (5, 'Alex', 'Kennedy');
+
+Insert INTO AvailableTimeslots (trainer_id, time_slot_hour) VALUES
+    (1, '09:00'),
+    (1, '10:00'),
+    (1, '11:00'),
+    (1, '12:00'),
+    (2, '13:00'),
+    (2, '14:00'),
+    (2, '15:00'),
+    (3, '16:00'),
+    (4, '17:00'),
+    (5, '08:00');
+
+INSERT INTO Rooms (room_number) VALUES
+    (114),
+    (115),
+    (116),
+    (117),
+    (214),
+    (215),
+    (216),
+    (217);
+
+INSERT INTO FitnessEvents (event_id, event_type, room_number, trainer_id, time_slot_hour) VALUES
+    (1, 'Personal Session', 114, 1, '09:00'),
+    (2, 'Personal Session', 114, 1, '10:00'),
+    (3, 'Personal Session', 115, 2, '14:00'),
+    (4, 'Group Class', 114, 3, '16:00'),
+    (5, 'Personal Session', 214, 4, '17:00');
+
+INSERT INTO EventBookings (booking_id, event_id, member_id) VALUES
+    (1, 1, 1),
+    (2, 2, 2),
+    (3, 4, 1),
+    (3, 4, 4),
+    (3, 4, 5);
+
+INSERT INTO AdministrativeStaff (admin_id, first_name, last_name) VALUES
+    (1, 'Alice', 'Griffin'),
+    (2, 'Avery', 'Quinn'),
+    (3, 'James', 'Beckett'),
+    (4, 'Logan', 'Sawyer'),
+    (5, 'Dylan', 'Wallace');
