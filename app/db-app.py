a71f752c5a72e75a299c2f56173c4b6ec1b171c0
Jacob Gaumond <jacobgaumond@hotmail.com>
2025-12-01 21:51:33 -0500

    Implements member registration and updating

--- app/db-app.py
+++ app/db-app.py
@@ -28,17 +28,43 @@ def executeQuery(db_query):
 Helper function used to prompt a user for function argument values.
 """
 def promptArguments(argumentList):
-    print("You have chosen an option that calls a function requiring arguments.")
-    print("Please enter the desired values for each argument\n")
+    print("You have chosen an option that requires input information.")
+    print("Please enter the desired values for each field\n")
 
     outputList = []
     for argument in argumentList:
         outputList += [input(argument + ": ")]
     
-    print("All arguments have been collected. Continuing...")
+    print("All fields have been collected. Continuing...")
     print(ASTERISK_STRING)
 
     return outputList
+
+def createMember(first_name, last_name, email, heart_rate_bpm, current_weight_lb, target_weight_lb):
+    # Create the member
+    db_query = "INSERT INTO Members (first_name, last_name, email) VALUES ('" + first_name + "', '" + last_name + "', '" + email + "');"
+    executeQuery(db_query)
+
+    # Initialize its HealthInfo entry
+    db_query = "INSERT INTO HealthInfo (member_id, heart_rate_bpm, current_weight_lb) VALUES ((SELECT member_id FROM Members WHERE first_name = '" + first_name + "' AND last_name = '" + last_name + "'), " + heart_rate_bpm + ", " + current_weight_lb + ");"
+    executeQuery(db_query)
+    
+    # Initialize its FitnessGoals entry
+    db_query = "INSERT INTO FitnessGoals (member_id, target_weight_lb) VALUES ((SELECT member_id FROM Members WHERE first_name = '" + first_name + "' AND last_name = '" + last_name + "'), " + target_weight_lb + ");"
+    executeQuery(db_query)
+
+def updateMember(first_name, last_name, email, heart_rate_bpm, current_weight_lb, target_weight_lb):
+    # Update the email
+    db_query = "UPDATE Members SET email = '" + email + "' WHERE first_name = '" + first_name + "' AND last_name = '" + last_name + "');"
+    executeQuery(db_query)
+
+    # Update the HealthInfo entry
+    db_query = "UPDATE HealthInfo SET heart_rate_bpm = " + heart_rate_bpm + ", current_weight_lb = " + current_weight_lb + " WHERE member_id = (SELECT member_id FROM Members WHERE first_name = '" + first_name + "' AND last_name = '" + last_name + "');"
+    executeQuery(db_query)
+
+    # Update the FitnessGoals entry
+    db_query = "UPDATE FitnessGoals SET target_weight_lb = " + target_weight_lb + " WHERE member_id = (SELECT member_id FROM Members WHERE first_name = '" + first_name + "' AND last_name = '" + last_name + "');"
+    executeQuery(db_query)
 
 # Main Code
 if __name__ == '__main__':
@@ -92,12 +118,25 @@ if __name__ == '__main__':
                 print("Entered option is not known. Try again.")
                 print(ASTERISK_STRING)
         elif (user_role == "Member"):
-            pass
-            # else:
-            #     print(ASTERISK_STRING)
+            if (user_input == "1"):
+                print("Registering new member. Enter desired member information.")
 
-            #     print("Entered option is not known. Try again.")
-            #     print(ASTERISK_STRING)
+                input_arguments = promptArguments(["first_name", "last_name", "email", "heart_rate_bpm", "current_weight_lb", "target_weight_lb"])
+                createMember(input_arguments[0], input_arguments[1], input_arguments[2], input_arguments[3], input_arguments[4], input_arguments[5])
+
+                print(ASTERISK_STRING)
+            elif (user_input == "2"):
+                print("Updating personal information. Enter first/last name, then enter new information.")
+
+                input_arguments = promptArguments(["first_name", "last_name", "email", "heart_rate_bpm", "current_weight_lb", "target_weight_lb"])
+                updateMember(input_arguments[0], input_arguments[1], input_arguments[2], input_arguments[3], input_arguments[4], input_arguments[5])
+
+                print(ASTERISK_STRING)
+            else:
+                print(ASTERISK_STRING)
+
+                print("Entered option is not known. Try again.")
+                print(ASTERISK_STRING)
         elif (user_role == "Trainer"):
             pass
             # else:
